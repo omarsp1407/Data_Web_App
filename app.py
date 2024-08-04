9d7ff5ed071e90d406c045ec5978316076e02972
omarsp1407 <omar.soto.perez.01@gmail.com>
2024-08-03 19:14:49 -0600

    ajustes de aplicacion

--- app.py
+++ app.py
@@ -10,13 +10,20 @@ st.header('Generador de Histogramas con Plotly Express')
 
 # Función para crear el histograma
 def crear_histograma():
-    datos = np.random.randn(1000)  
-    df = pd.DataFrame({'Valores': datos})
-    fig = px.histogram(df, x='Valores', nbins=30, title='Histograma de Valores Aleatorios')
-    
+    fig = px.histogram(car_data, x="odometer") # crear un histograma
+    return fig
+
+# Función para crear el gráfico de dispersión
+def crear_grafico_dispersion():
+    fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión    
     return fig
 
 # Crear un botón que al hacer clic construye el histograma
 if st.button('Generar Histograma'):
     histograma = crear_histograma()
     st.plotly_chart(histograma)
+
+# Crear un botón que al hacer clic construye el diagraa de dispersion
+if st.button('Generar Histograma'):
+    dispersion = crear_grafico_dispersion()
+    st.plotly_chart(dispersion)
